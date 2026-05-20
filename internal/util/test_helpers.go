870e4e659f653e8f9cc6361928d71108094c1e90
Fergal Mc Carthy <fmccarthy@suse.com>
2026-05-20 16:39:04 -0400

    WIP

--- internal/util/test_helpers.go
+++ internal/util/test_helpers.go
@@ -5,6 +5,8 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/mock"
 )
 
 func ReadTestFile(name string, t *testing.T) []byte {
@@ -28,4 +30,69 @@ func TestContentMatches(t *testing.T, expected string, got string) {
 			"---"}
 		t.Errorf(strings.Join(message, "\n"), expected, got)
 	}
+}
+
+//
+// Helpers for mocking Execute()
+//
+
+type MockExecutor struct {
+	mock.Mock
+}
+
+func NewMockExecutor() *MockExecutor {
+	return &MockExecutor{}
+}
+
+func (m *MockExecutor) Setup(t *testing.T) func() {
+	// retrieve the original Execute
+	origExecute := Execute
+
+	// define a teardown function
+	teardown := func() {
+		// restore the original Execute handler
+		Execute = origExecute
+
+		// check that expectations were met, such as calls being made
+		// or only
+		m.Mock.AssertExpectations(t)
+	}
+
+	// replace the original Execute with the Mock handler
+	Execute = m.Execute
+
+	return teardown
+}
+
+func (m *MockExecutor) Execute(cmd []string, validExitCodes []int) (output []byte, err error) {
+	// attempt to retrieve any return values previously registered for this
+	// call signature; will panic if matching call signature not registered
+	args := m.Called(cmd, validExitCodes)
+
+	// extract output return value if provided
+	if args.Get(0) != nil {
+		output = args.Get(0).([]byte)
+	}
+
+	// extract error return value if provided
+	if args.Get(1) != nil {
+		err = args.Get(1).(error)
+	}
+
+	return
+}
+
+// compile time validation that MockExecutor.Execute matches ExecuteFunc
+var _ ExecuteFunc = (*MockExecutor)(nil).Execute
+
+// add an On("Execute", ...) mocking directive, returning a mock.Call
+// that can be manipulated with standard mock.Call methods, such as
+// Once(), Maybe(), etc.
+func (m *MockExecutor) OnExecute(cmd []string, validExitCodes []int) *mock.Call {
+	return m.On("Execute", cmd, validExitCodes)
+}
+
+// simplified wrapper to quickly setup a simple call with return values
+func (m *MockExecutor) OnExecuteReturn(cmd []string, validExitCodes []int, output []byte, err error) *mock.Call {
+	return m.OnExecute(cmd, validExitCodes).Return(output, err)
 }
